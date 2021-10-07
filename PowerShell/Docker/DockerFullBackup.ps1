10682c654ced17efe8f8348f420c89e30c8958c7
Daniel Seara <Daniels@ndsoft.info>
2021-10-07 13:10:45 -0300

    Add files via upload

---
+++ PowerShell/Docker/DockerFullBackup.ps1
@@ -0,0 +1,21 @@
+﻿Import-Module -Name D:\Desarrollos\PowerShell\Docker\DockerLib.psm1
+d:
+cd D:\Desarrollos\Docker\Backup
+# Look for Docker Desktop instance
+$DockerProcesses=Get-Process|Where-Object {$_.ProcessName -eq 'Docker Desktop'}
+if($DockerProcesses.count -eq 0) #If there is no instance, try to start it
+{
+    $command="C:\Program Files\Docker\Docker\Docker Desktop.exe"
+    Invoke-Expression "& '$command'"
+    while($DockerProcesses.count -eq 0) #Whait for an instance
+    {
+        Start-Sleep -Seconds 120
+        $DockerProcesses=Get-Process|Where-Object {$_.ProcessName -eq 'Docker Desktop'}
+    }
+}
+$Result=docker ps -a
+for($i=1;$i -lt $result.count;$i++)
+{
+
+    Backup-Container $result[$i]
+}
---
+++ PowerShell/Docker/DockerLib.psm1
@@ -0,0 +1,22 @@
+﻿function Backup-Container
+{
+[CmdletBinding()]
+param
+(
+	[Parameter(Mandatory=$true,Position=0,HelpMessage="Must define the docker container name" )]
+	[string]$Line
+
+)
+
+	 $SpacePos=$Line.IndexOf(' ') # Find the first space to retrieve the ContainerID
+	 $ContainerID=$Line.Substring(0,$SpacePos)
+	 $SpacePos=$Line.LastIndexOf(' ') # Find the last space, where starts the container's name
+	 $name=$Line.Substring($SpacePos)
+	 Write-host "Backing up $name"
+	 $Name=$Name.ToLower()+"bkp" # Add 'bkp' the the container's name as file name for the backup
+	 $command="docker commit  $ContainerID $name" # First, create a commited version of the container
+	 Invoke-Expression $command
+	 $namebak="$name.tar"
+	 $command="docker save -o $namebak $Name" # Then, save the commited one to disk
+	 Invoke-Expression $command
+}
